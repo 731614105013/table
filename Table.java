c7c375be2301431300a12ee1b3b148b3e47266ca
731614105013 <gomathiammupk29@gmail.com>
2017-07-06 13:59:57 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,22 @@
+# table
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Table{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int a,b,c,i;
+    a=s.nextInt();
+    c=s.nextInt();
+    for(i=1;i<=c;i++)
+    {
+       b=a*i;
+       System.out.println(a+"*"+i+"="+b);
+       }
+       }
+       }
+       
+ 
+
+
